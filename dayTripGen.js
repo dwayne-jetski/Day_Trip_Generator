80f53ccf8b7f57dc9636fc1e078ef04f59a4df4e
marcus-baumgardner <dwayne.jerski@gmail.comgit config --global user.name marcus-baumgardnergit config --global user.email dwayne.jerski@gmail.com>
2020-10-19 20:21:56 -0500

    added conditions for changing trip details

--- dayTripGen.js
+++ dayTripGen.js
@@ -2,7 +2,7 @@
 
 let destinations = ["London, England", "New York City, New York, USA", "Paris, France", "Tokyo, Japan", "Moscow, Russia", "Dubai, United Arab Emirates"];
 let restaurant = ["Mcdonalds", "Burger King", "Qdoba", "Arby's", "Chili's", "Applebee's"];
-let transportation = ["walk", "fly in an airplane", "ride a bicycle", "drive a car", "hitchhike", "runing"];
+let transportation = ["walk", "airplane", "bicycle", "car", "hitchhiking", "running"];
 let activities = ["go for a walk", "visit a Museum", "go to a bar", "stay in and watch TV", "do a crossword puzzle"];
 
 
@@ -16,25 +16,41 @@ let userInput = "";
 
     while(true){
         userInput = prompt("Would you like to change anything about your trip? yes/no:");
-        userInput.toLowerCase();
-        if(userInput.charAt(0) !== "y" || userInput.charAt(0) !== "n"){
-
+         userInput = userInput.toLowerCase();
+        if(userInput.charAt(0) === "y"){
+            break
+        }
+        else if(userInput.charAt(0) === "n"){
+            break
         }
         else{
-            break
+
         }
     }
 
     if(userInput.charAt(0) === "y"){
-        
+        userInput = prompt("What would you like to change?: \n1) Destination \n2) Transportation \n3) Restaruant \n4) Entertainment")
+        userInput = parseInt(userInput);
+        if(userInput === 1){
+            userTrip[0] = destinations[Math.floor(Math.random() * destinations.length)];
+        }
+        else if(userInput === 2){
+            userTrip[1] = transportation[Math.floor(Math.random() * transportation.length)];
+        }
+        else if(userInput === 3){
+            userTrip[2] = restaurant[Math.floor(Math.random() * restaurant.length)];
+        }
+        else if(userInput === 4){
+            userTrip[3] = activities[Math.floor(Math.random() * activities.length)];
+        }
     }
     else{
         userInput = prompt("Does your trip look good then? yes/no: ");
-        if(userInput.charAt(0) !== "y" || userInput.charAt(0) !== "n"){
-
+        if(userInput.charAt(0) === "y"){
+            break;
         }
         else{
-            break;
+            
         }
     }
 } 
