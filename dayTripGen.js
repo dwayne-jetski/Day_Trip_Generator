587bd14b2c50c521723cce3bf214f9024c2f7f71
marcus-baumgardner <dwayne.jerski@gmail.comgit config --global user.name marcus-baumgardnergit config --global user.email dwayne.jerski@gmail.com>
2020-10-19 19:20:15 -0500

    array for destination, restaurant, transportation, activities, userTrip added

--- dayTripGen.js
+++ dayTripGen.js
@@ -1,1 +1,9 @@
 "use Strict"
+
+let destinations = ["London, England", "New York City, New York, USA", "Paris, France", "Tokyo, Japan", "Moscow, Russia", "Dubai, United Arab Emirates"];
+let restaurant = ["Mcdonalds", "Burger King", "Qdoba", "Arby's", "Chili's", "Applebee's"];
+let transportation = ["walk", "fly in an airplane", "ride a bicycle", "drive a car", "hitchhike", "run"];
+let activities = ["go for a walk", "visit a Museum", "go to a bar", "stay in and watch TV", "do a crossword puzzle"];
+
+
+let userTrip = [];
